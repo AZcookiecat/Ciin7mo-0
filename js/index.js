93025748bb2860e10c45fdfd443a6de9ee7c26a5
18821959907 <18821959907@163.com>
2025-03-21 21:16:30 +0800

    no message

--- js/index.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    var welcome = document.getElementById("welcome");
-    welcome.style.display = "block";
-    setTimeout(function() {
-        welcome.style.display = "none";
-    }, 3000);
-};
